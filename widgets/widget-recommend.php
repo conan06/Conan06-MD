4fce1d3ec8d4a1b2c408eb326686b7cfe5ad96f2
Schwinn Hung <conan06@126.com>
2016-12-14 22:40:54 +0800

    添加顶部widget
    添加顶部widget，后续：增加自定义摘要，免去文章修改。

--- widgets/widget-recommend.php
+++ widgets/widget-recommend.php
@@ -7,14 +7,6 @@
  * @link http://conan06.com
  */
 
-function get_mdl_recommend_article( $initial = true, $echo = true ) {
-    
-
-    return apply_filters( 'get_mdl_recommend_article', $recommend_output );
-}
-
-/*******************************************************************************/
-
 class conanMD_Recommend_Widget extends WP_Widget {
 
 	
@@ -37,13 +29,52 @@ class conanMD_Recommend_Widget extends WP_Widget {
 	 * --------------------------------------------------------------------------- */
 	function widget( $args, $instance ) {
 
+		// Get the latest post or the user-defined post
+		$post_id = apply_filters( 'widget_post_id', empty( $instance['post'] ) ? 
+				wp_get_recent_posts( array( 'numberposts' => '1') )[0]['ID'] : $instance['post'] );
+
 		echo $args['before_widget'];
-		if ( $title ) {
-			echo $args['before_title'] . $title . $args['after_title'];
+		
+		if (!empty($post_id)) {
+			echo '<div id="cmd-recommend-wrap" class="cmd-recommend-wrap">';
+			//TODO
+			$article = get_post($post_id);
+			$output = "\n\t\t".'
+			<div class="cmd-recommend-content">
+				<figure>';
+
+			if ( has_post_thumbnail($article) ) {
+				$output .= '<div class="cmd-recommend-background">'. get_the_post_thumbnail( $article, 'full' ) . '</div>';
+			}
+
+			$output .= "\n\t\t".'
+				</figure>
+				<section>
+					<div class="cmd-recommend-meta">';
+			$output .= '<time datetime="' . $article->post_date . '">' . get_the_date( '', $article ) . '</time>';
+
+			$categories = get_the_category( $post_id );
+			if ( ! empty( $categories ) ) {
+				foreach( $categories as $category ) {
+					$output .= '<a href="' . esc_url( get_category_link( $category->term_id ) ) . '" rel="category tag">' . esc_html( $category->name ) . '</a>';
+				}
+			}
+			
+			$output .= "\n\t\t".'
+					</div>
+					<a href="' . get_permalink( $article ) . '" class="cmd-recommend-link">
+						<h1 class="cmd-recommend-title">' . $article->post_title . '</h1>
+						<span class="cmd-recommend-summary">' . $article->post_excerpt . '</span>
+						<span class="cmd-recommend-read-more">' . sprintf( __( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'ConanMD' ), $article->post_title ) . '</span>
+					</a>';
+			$output .= "\n\t\t".'
+				</section>
+			</div>';
+
+			echo $output;
+			echo '</div>';
 		}
-		echo '<div id="cmd-recommend-wrap" class="cmd-recommend-wrap">';
-		get_mdl_recommend_article();
-		echo '</div>';
+		
 		echo $args['after_widget'];
 
 	}
