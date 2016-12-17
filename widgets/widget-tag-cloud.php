71392d94e9af780926b1c1b425e5114d45062a6a
Schwinn Hung <conan06@126.com>
2016-12-17 17:03:11 +0800

    增加tag数，卡片切换

--- widgets/widget-tag-cloud.php
+++ widgets/widget-tag-cloud.php
@@ -7,12 +7,13 @@
  * @link http://conan06.com
  */
 
-function get_mdl_tag_cloud() {
-	$args = array(
-		'smallest' => 8, 'largest' => 22, 'unit' => 'pt', 'number' => 16,
+function get_mdl_tag_cloud( $args = '' ) {
+	$defaults = array(
+		'smallest' => 8, 'largest' => 22, 'unit' => 'pt', 'number' => 24,
 		'format' => 'flat', 'separator' => "\n", 'orderby' => 'count', 'order' => 'DESC',
 		'exclude' => '', 'include' => '', 'link' => 'view', 'post_type' => ''
 	);
+    $args = wp_parse_args( $args, $defaults );
 
 	$tags = get_terms( 'post_tag', $args ); // Get tags
 
@@ -71,20 +72,31 @@ class conanMD_Tag_Cloud_Widget extends WP_Widget {
 	 * --------------------------------------------------------------------------- */
 	function widget( $args, $instance ) {
 
-		$tag_cloud = get_mdl_tag_cloud();
+        $title = apply_filters( 'widget_title', empty( $instance['title'] ) ? '' : $instance['title'], $instance, $this->id_base );
+        $number = ( ! empty( $instance['number'] ) ) ? absint( $instance['number'] ) : 24;
+        $card = ! empty( $instance['card'] ) ? '1' : '0';
+
+        if ( ! $number )
+			$number = 24;
+
+		$tag_cloud = get_mdl_tag_cloud( apply_filters( 'widget_tag_cloud_args', array(
+			'number' => $number
+		) ) );
 
 		if ( empty( $tag_cloud ) ) {
 			return;
 		}
-
-		$title = apply_filters( 'widget_title', empty( $instance['title'] ) ? '' : $instance['title'], $instance, $this->id_base );
 
 		echo $args['before_widget'];
 		if ( $title ) {
 			echo $args['before_title'] . $title . $args['after_title'];
 		}
 
-		echo '<div class="mdl-card mdl-shadow--2dp cmd-widget-tag-cloud-wrap">';
+        if ( $card ) {
+            echo '<div class="mdl-card mdl-shadow--2dp cmd-widget-tag-cloud-wrap">';
+        } else {
+            echo '<div class="cmd-widget-tag-cloud-wrap">';
+        }
 
 		echo $tag_cloud;
 
@@ -101,6 +113,8 @@ class conanMD_Tag_Cloud_Widget extends WP_Widget {
 		
         $instance = $old_instance;
 		$instance['title'] = sanitize_text_field( $new_instance['title'] );
+        $instance['number'] = (int) $new_instance['number'];
+        $instance['card'] = $new_instance['card'] ? 1 : 0;
 
 		return $instance;
 	}
@@ -111,12 +125,21 @@ class conanMD_Tag_Cloud_Widget extends WP_Widget {
 	 * --------------------------------------------------------------------------- */
 	function form( $instance ) {
 		
-		$instance = wp_parse_args( (array) $instance, array( 'title' => '' ) );
+		$instance = wp_parse_args( (array) $instance, array( 'title' => '', 'card' => 0 ) );
 		$title = sanitize_text_field( $instance['title'] );
+        $number = isset( $instance['number'] ) ? absint( $instance['number'] ) : 24;
 		?>
 		<p>
 			<label for="<?php echo $this->get_field_id('title'); ?>"><?php _e('Title:'); ?></label>
 			<input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo esc_attr($title); ?>" />
+		</p>
+        <p>
+            <label for="<?php echo $this->get_field_id( 'number' ); ?>"><?php _e( 'Number of tags to show:', 'ConanMD' ); ?></label>
+		    <input class="tiny-text" id="<?php echo $this->get_field_id( 'number' ); ?>" name="<?php echo $this->get_field_name( 'number' ); ?>" type="number" step="1" min="1" value="<?php echo $number; ?>" size="3" />
+        </p>
+        <p>
+			<input class="checkbox" type="checkbox"<?php checked( $instance['card'] ); ?> id="<?php echo $this->get_field_id('card'); ?>" name="<?php echo $this->get_field_name('card'); ?>" />
+            <label for="<?php echo $this->get_field_id('card'); ?>"><?php _e( 'Use material design card', 'ConanMD' ); ?></label>
 		</p>
 		<?php
 	}
