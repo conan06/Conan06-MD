f34ed064a223467455dc9f1b6ebfea8152e5892a
Schwinn Hung <conan06@126.com>
2016-12-20 14:27:40 +0800

    card头图改为16:9
    Rich media 官方推荐 16:9 or 1:1 aspect ratio

--- widgets/widget-profile.php
+++ widgets/widget-profile.php
@@ -30,6 +30,8 @@ class conanMD_Profile_Widget extends WP_Widget {
 	function widget( $args, $instance ) {
 
         $title = apply_filters( 'widget_title', empty( $instance['title'] ) ? '' : $instance['title'], $instance, $this->id_base );
+        $user_id = 1;
+        $user_job = 'Student';
 
 		echo $args['before_widget'];
 		if ( $title ) {
@@ -38,23 +40,40 @@ class conanMD_Profile_Widget extends WP_Widget {
 
         echo '<div class="mdl-card mdl-shadow--2dp cmd-widget-profile-wrap">';
 
-		echo '<div class="mdl-card__title">
-    <h2 class="mdl-card__title-text">Welcome</h2>
-  </div>
-  <div class="mdl-card__supporting-text">
-    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-    Mauris sagittis pellentesque lacus eleifend lacinia...
-  </div>
-  <div class="mdl-card__actions mdl-card--border">
-    <a class="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect">
-      Get Started
-    </a>
-  </div>
-  <div class="mdl-card__menu">
-    <button class="mdl-button mdl-button--icon mdl-js-button mdl-js-ripple-effect">
-      <i class="material-icons">share</i>
-    </button>
-  </div>';
+		echo '<div class="cmd-widget-profile-header" style="background-image: url('. get_header_image() .');"></div>
+            <div class="cmd-widget-profile-social-wrap">
+                ' . get_avatar( get_the_author_meta( 'user_email', $user_id ), 72 ) . '
+                <div class="cmd-widget-profile-social">
+                    <svg class="icon icon-facebook" aria-hidden="true" role="img">
+                        <use href="#icon-facebook" xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#icon-facebook"></use>
+                    </svg>
+                    <svg class="icon icon-twitter" aria-hidden="true" role="img">
+                        <use href="#icon-twitter" xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#icon-twitter"></use>
+                    </svg>
+                    <svg class="icon icon-github" aria-hidden="true" role="img">
+                        <use href="#icon-github" xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#icon-github"></use>
+                    </svg>
+                    <svg class="icon icon-instagram" aria-hidden="true" role="img">
+                        <use href="#icon-instagram" xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#icon-instagram"></use>
+                    </svg>
+                    <svg class="icon icon-youtube" aria-hidden="true" role="img">
+                        <use href="#icon-youtube" xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#icon-youtube"></use>
+                    </svg>
+                </div>
+            </div>';
+
+        echo '<div class="mdl-card__title cmd-widget-profile-title">';
+        echo '<h3 class="mdl-card__title-text">'. get_the_author_meta( 'user_nicename', $user_id ) . '</h3>';
+        echo '<h6 class="mdl-card__subtitle-text">'. $user_job .'</h6>';
+        echo "</div>\n";
+        
+        echo '<div class="mdl-card__supporting-text">'. get_the_author_meta( 'description', $user_id ) .'</div>';
+
+        echo '<div class="mdl-card__actions mdl-card--border">
+                <a class="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect" href="' . esc_url( get_bloginfo( 'rss2_url' ) ) . '">' 
+                . __('Subscribe entries', 'ConanMD')
+                . '</a>';
+        echo "</div>\n";
 
 		echo "</div>\n";
 		echo $args['after_widget'];
