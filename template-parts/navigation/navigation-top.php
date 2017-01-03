34a77fb5eb12fe3c849e0e7380299506a45bd139
Schwinn Hung <conan06@126.com>
2017-01-03 20:51:16 +0800

    修改menu，添加站点logo

--- template-parts/navigation/navigation-top.php
+++ template-parts/navigation/navigation-top.php
@@ -9,6 +9,12 @@
  */
 
 ?>
+<a href="<?php printf( site_url() ) ?>" class="site-logo">
+     <figure>
+        <img src="<?php printf( esc_url( get_site_icon_url( 64 ) ) ) ?>" alt=" Google">
+    </figure>
+</a><!-- .site-logo -->
+
 <nav id="site-navigation" class="main-navigation" role="navigation" aria-label="<?php _e( 'Top Menu', 'ConanMD' ); ?>">
 	<button class="menu-toggle" aria-controls="top-menu" aria-expanded="false"><?php echo conanMD_get_svg( array( 'icon' => 'bars' ) ); echo conanMD_get_svg( array( 'icon' => 'close' ) ); _e( 'Menu', 'ConanMD' ); ?></button>
 	<?php wp_nav_menu( array(
@@ -20,3 +26,13 @@
 		<a href="#content" class="menu-scroll-down"><?php echo conanMD_get_svg( array( 'icon' => 'arrow-right' ) ); ?><span class="screen-reader-text"><?php _e( 'Scroll down to content', 'ConanMD' ); ?></span></a>
 	<?php endif; ?>
 </nav><!-- #site-navigation -->
+
+<div class="navigation-more-menu-wrapper">
+	<button id="navigation-more-menu" class="mdl-button mdl-js-button mdl-button--icon">
+		<i class="material-icons">more_vert</i>
+	</button>
+
+	<ul class="mdl-menu mdl-menu--bottom-right mdl-js-menu mdl-js-ripple-effect" for="navigation-more-menu">
+		<li class="mdl-menu__item">RSS</li>
+	</ul>
+</div>
