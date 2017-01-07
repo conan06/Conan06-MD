9f38fdeaf5684c65da830d434ced0dcdbccc8a87
Schwinn Hung <conan06@126.com>
2017-01-07 22:45:53 +0800

    add simple search bar in menu

--- template-parts/navigation/navigation-top.php
+++ template-parts/navigation/navigation-top.php
@@ -65,19 +65,34 @@ function mdl_loginout($redirect = '', $echo = true) {
 </nav><!-- #site-navigation -->
 
 <div class="navigation-more-menu-wrapper">
+	<div class="navigation-more-menu-container">
 
-	<button id="navigation-search" class="mdl-button mdl-js-button mdl-button--icon">
-		<i class="material-icons">search</i>
-	</button>
+		<form role="search" method="get" class="search-form" action="<?php echo esc_url( home_url( '/' ) ); ?>">
+			<div class="mdl-textfield mdl-js-textfield mdl-textfield--expandable mdl-textfield--align-right">
+				<label class="mdl-button mdl-js-button mdl-button--icon" for="navigation-search">
+					<i class="material-icons">search</i>
+				</label>
+				<div class="mdl-textfield__expandable-holder">
+					<input id="navigation-search"
+							class="mdl-textfield__input"
+							type="search"
+							placeholder="<?php echo esc_attr_x( 'Search &hellip;', 'placeholder', 'ConanMD' ); ?>"
+							value="<?php echo get_search_query(); ?>"
+							name="s">
+				</div>
+			</div>
+		</form>
 
-	<button id="navigation-more-menu" class="mdl-button mdl-js-button mdl-button--icon">
-		<i class="material-icons">more_vert</i>
-	</button>
+		<button id="navigation-more-menu" class="mdl-button mdl-js-button mdl-button--icon">
+			<i class="material-icons">more_vert</i>
+		</button>
 
-	<div class="mdl-menu mdl-menu--bottom-right mdl-js-menu mdl-js-ripple-effect" for="navigation-more-menu">
-		<?php mdl_register(); ?>
-		<?php mdl_loginout(); ?>
-		<a class="mdl-menu__item" href="<?php echo esc_url( get_bloginfo( 'rss2_url' ) ); ?>"><i class="material-icons">rss_feed</i><?php _e('Entries <abbr title="Really Simple Syndication">RSS</abbr>'); ?></a>
-		<a class="mdl-menu__item" href="<?php echo esc_url( get_bloginfo( 'comments_rss2_url' ) ); ?>"><i class="material-icons">rss_feed</i><?php _e('Comments <abbr title="Really Simple Syndication">RSS</abbr>'); ?></a>
+		<div class="mdl-menu mdl-menu--bottom-right mdl-js-menu mdl-js-ripple-effect" for="navigation-more-menu">
+			<?php mdl_register(); ?>
+			<?php mdl_loginout(); ?>
+			<a class="mdl-menu__item" href="<?php echo esc_url( get_bloginfo( 'rss2_url' ) ); ?>"><i class="material-icons">rss_feed</i><?php _e('Entries <abbr title="Really Simple Syndication">RSS</abbr>'); ?></a>
+			<a class="mdl-menu__item" href="<?php echo esc_url( get_bloginfo( 'comments_rss2_url' ) ); ?>"><i class="material-icons">rss_feed</i><?php _e('Comments <abbr title="Really Simple Syndication">RSS</abbr>'); ?></a>
+		</div>
+
 	</div>
 </div>
