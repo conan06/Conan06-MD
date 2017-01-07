886002ee3ad2b83dac0f32c32ec7def9bdd6d90b
Schwinn Hung <conan06@126.com>
2017-01-07 18:08:07 +0800

    top-menu

--- template-parts/navigation/navigation-top.php
+++ template-parts/navigation/navigation-top.php
@@ -8,6 +8,40 @@
  * @version 1.0
  */
 
+function mdl_register( $echo = true ) {
+    if ( ! is_user_logged_in() ) {
+        if ( get_option('users_can_register') )
+            $link = '<a class="mdl-menu__item" href="' . esc_url( wp_registration_url() ) . '"><i class="material-icons">person_add</i>' . __('Register') . '</a>';
+        else
+            $link = '';
+    } elseif ( current_user_can( 'read' ) ) {
+        $link = '<a class="mdl-menu__item" href="' . admin_url() . '"><i class="material-icons">settings</i>' . __('Site Admin') . '</a>';
+    } else {
+        $link = '';
+    }
+
+    $link = apply_filters( 'register', $link );
+ 
+    if ( $echo ) {
+        echo $link;
+    } else {
+        return $link;
+    }
+}
+
+function mdl_loginout($redirect = '', $echo = true) {
+    if ( ! is_user_logged_in() )
+        $link = '<a class="mdl-menu__item mdl-menu__item--full-bleed-divider" href="' . esc_url( wp_login_url($redirect) ) . '"><i class="material-icons">person</i>' . __('Log in') . '</a>';
+    else
+        $link = '<a class="mdl-menu__item mdl-menu__item--full-bleed-divider" href="' . esc_url( wp_logout_url($redirect) ) . '"><i class="material-icons">exit_to_app</i>' . __('Log out') . '</a>';
+ 
+    if ( $echo ) {
+        echo apply_filters( 'loginout', $link );
+    } else {
+        return apply_filters( 'loginout', $link );
+    }
+}
+
 ?>
 <a href="<?php printf( site_url() ) ?>" class="site-logo">
      <figure>
@@ -32,7 +66,10 @@
 		<i class="material-icons">more_vert</i>
 	</button>
 
-	<ul class="mdl-menu mdl-menu--bottom-right mdl-js-menu mdl-js-ripple-effect" for="navigation-more-menu">
-		<li class="mdl-menu__item">RSS</li>
-	</ul>
+	<div class="mdl-menu mdl-menu--bottom-right mdl-js-menu mdl-js-ripple-effect" for="navigation-more-menu">
+		<?php mdl_register(); ?>
+		<?php mdl_loginout(); ?>
+		<a class="mdl-menu__item" href="<?php echo esc_url( get_bloginfo( 'rss2_url' ) ); ?>"><i class="material-icons">rss_feed</i><?php _e('Entries <abbr title="Really Simple Syndication">RSS</abbr>'); ?></a>
+		<a class="mdl-menu__item" href="<?php echo esc_url( get_bloginfo( 'comments_rss2_url' ) ); ?>"><i class="material-icons">rss_feed</i><?php _e('Comments <abbr title="Really Simple Syndication">RSS</abbr>'); ?></a>
+	</div>
 </div>
